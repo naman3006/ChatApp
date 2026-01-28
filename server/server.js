7d50c92e8653eea275b11f877d39768a53590da7
Naman Patel <namanpatel806@gmail.com>
2026-01-28 10:32:38 +0530

    add url

--- server/server.js
+++ server/server.js
@@ -32,9 +32,10 @@ app.use(
         origin: [
             "http://localhost:3000",
             "http://localhost:5173",
-            "https://chat-app-topaz-phi-50.vercel.app" ||
+            "https://chat-app-topaz-phi-50.vercel.app",
             "https://chat-zzqxrm3l3-naman-patels-projects-b7bc3e39.vercel.app",
-            // ...(process.env.CLIENT_URL ? [process.env.CLIENT_URL] : []),
+            "https://chat-app-git-main-naman-patels-projects-b7bc3e39.vercel.app",
+            ...(process.env.CLIENT_URL ? [process.env.CLIENT_URL] : []),
         ],
         credentials: true,
     })
